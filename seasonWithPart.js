57382cbfbb4e856aad463f3a590380e7e8924ce8
Francisco Matamoros <francis.matamoros@hotmail.com>
2022-05-05 19:02:15 +0200

    test con andrés

--- seasonWithPart.js
+++ seasonWithPart.js
@@ -3,7 +3,15 @@ const console = new Console();
 let day = 0;
 let month = 0;
 let year = 0;
-const FIRST_DAY_SEASON = 21;
+// primero de primavera
+const FIRST_DAY_SPRING = 3 * 30 + 21;
+// primero de invierno
+const FIRST_DAY_SUMMER = 6 * 30 + 21;
+// primero de otoño
+const FIRST_DAY_AUTUMN = 9 * 30 + 21;
+// primero de invierno
+const FIRST_DAY_WINTER = 11 * 30 + 21;
+
 do {
     day = console.readNumber(`Escriba un día (1-30):`);
 } while (day < 1 || day > 30);
@@ -14,7 +22,26 @@ do {
     year = console.readNumber(`Escriba un año (1-...):`);
 } while (year < 1);
 let msg = `El día ${day} del ${month} de ${year} cae a `;
-if ()
+let msgSeason = ``;
+let dayOfYear = day + (month - 1) * 30;
+console.writeln(dayOfYear);
+if (dayOfYear < FIRST_DAY_SPRING || dayOfYear >= FIRST_DAY_WINTER) {
+    msgSeason += `invierno`;
+}
+else {
+    if (dayOfYear < FIRST_DAY_SUMMER) {
+        msgSeason += `primavera`;
+    }
+    else {
+        if (dayOfYear < FIRST_DAY_AUTUMN) {
+            msgSeason += `verano`;
+        }
+        else {
+            msgSeason += `otoño`;
+        }
+    }
+}
+msg += msgSeason;
 
 console.writeln(`${msg}`);
 
